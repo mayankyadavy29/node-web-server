0c319a7e3db0670620cd43de0b64e7150171adc8
Mayank <mayankyadavy29@gmail.com>
2019-10-10 00:38:15 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express =require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.use((req, res, next) =>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}\n`;
+    console.log(log);
+    fs.appendFile('server.log', log, (err) => {
+        if(err){
+            console.log("Unable to log into server.log file");
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintain.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials/');
+
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle : 'Home Page',
+        welcomeMessage : 'Welcome to the home page'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle : 'About Page',
+        welcomeMessage : 'Welcome to the about page !!'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage : 'Error 404 not found !!'
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Started server!!");
+});
+
+
+
+
+
